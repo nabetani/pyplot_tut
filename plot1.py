1ee34dcf85b732159770b9aef13b61568a2910a1
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-02-08 16:36:11 +0900

    複数ページ

--- plot1.py
+++ plot1.py
@@ -3,15 +3,17 @@ import numpy as np
 from matplotlib import pyplot
 from matplotlib.backends.backend_pdf import PdfPages
 
-x = np.linspace(0, 2 * np.pi, 100)
-y1 = np.sin(x)
-y2 = np.cos(x)
 
-fig = pyplot.figure()
-axis1 = fig.add_subplot(2, 1, 1)  # nrows, ncols, index
-axis1.plot(x, y1)
-axis2 = fig.add_subplot(2, 1, 2)  # nrows, ncols, index
-axis2.plot(x, y2)
+for page_ix in range(3):
+    x = np.linspace(0, 2 * np.pi, 400)
+    t = np.power(x, page_ix+1)
+    y1 = np.sin(t)
+    y2 = np.cos(t)
+    fig = pyplot.figure()
+    axis1 = fig.add_subplot(2, 1, 1)  # nrows, ncols, index
+    axis1.plot(x, y1)
+    axis2 = fig.add_subplot(2, 1, 2)  # nrows, ncols, index
+    axis2.plot(x, y2)
 
 pdf = PdfPages('test2.pdf')
 
